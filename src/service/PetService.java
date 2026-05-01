b1531618cbfef6093f73735e6332b00df97bf23e
Neto <rneto879@gmail.com>
2026-05-01 18:16:14 -0300

    Construindo a parte de cadastros do sistema

--- src/service/PetService.java
+++ src/service/PetService.java
@@ -9,6 +9,14 @@ public class PetService {
 
     List<Pet> petList = new ArrayList<>();
 
+    public void addPetList(Pet pet){
+        petList.add(pet);
+    }
+
+    public void removePetList(Pet pet){
+        petList.remove(pet);
+    }
+
     public boolean hasSurname(String name){
         if(!name.contains(" ")){
             return false;
